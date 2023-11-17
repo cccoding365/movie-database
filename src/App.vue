c6ab6f4a103a7e5eeb89aacc9836b889d05e9dc4
DoubledConG <992196871@qq.com>
2023-11-17 22:05:20 +0800

    init: initial the movie database project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+	<h1>Movie database</h1>
+</template>
+
+<style scoped lang="less">
+h1{
+    color: #FFF;
+    font-family: 'Ubuntu', sans-serif;
+}
+</style>
