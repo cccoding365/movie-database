5759e46463d4abed5247452dac911694ecd4cea3
DoubledConG <992196871@qq.com>
2023-11-18 20:40:31 +0800

    feat: add axios interceptors

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,38 @@
-import axios from "axios";
+import Axios from "axios";
 import config from "@/configs/axios";
 
-export default axios.create({
+const axios = Axios.create({
 	baseURL: config.baseURL,
 	params: {
 		api_key: config.MOVIE_DB_API_KEY,
 		language: "en-US",
 	},
 });
+
+// Add a request interceptor
+axios.interceptors.request.use(
+	config => {
+		// Do something before request is sent
+		return config;
+	},
+	error => {
+		// Do something with request error
+		return Promise.reject(error);
+	},
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+	response => {
+		if (response.status === 200) {
+			return response.data;
+		} else {
+			return Promise.resolve({});
+		}
+	},
+	error => {
+		return Promise.reject(error);
+	},
+);
+
+export default axios;
