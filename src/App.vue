789f4a7e38c42ba8560dde31151075ea330c13d0
DoubledConG <992196871@qq.com>
2023-11-19 16:50:27 +0800

    feat: modify the function import method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-// import { onMounted } from "vue";
-import {
-	getMovies,
-	getGenres,
-	searchMovies,
-	getMovie,
-	getMovieCredits,
-	getMovieImages,
-	getMovieRecommendations,
-} from "@/apis";
+import * as Apis from "@/apis";
 
 const onGetMovies = async () => {
-	const data = await getMovies({
+	const data = await Apis.getMovies({
 		filter: "popular",
 		page: 1,
 	});
@@ -19,12 +10,12 @@ const onGetMovies = async () => {
 };
 
 const onGetGenres = async () => {
-	const data = await getGenres();
+	const data = await Apis.getGenres();
 	console.log(data);
 };
 
 const onSearchMovies = async () => {
-	const data = await searchMovies({
+	const data = await Apis.searchMovies({
 		query: "叶问",
 		page: 1,
 	});
@@ -32,22 +23,22 @@ const onSearchMovies = async () => {
 };
 
 const onGetMovie = async () => {
-	const data = await getMovie(872585);
+	const data = await Apis.getMovie(872585);
 	console.log(data);
 };
 
 const onMovieCredits = async () => {
-	const data = await getMovieCredits(872585);
+	const data = await Apis.getMovieCredits(872585);
 	console.log(data);
 };
 
 const onGetMovieImages = async () => {
-	const data = await getMovieImages(872585);
+	const data = await Apis.getMovieImages(872585);
 	console.log(data);
 };
 
 const onGetMovieRecommendations = async () => {
-	const data = await getMovieRecommendations(872585);
+	const data = await Apis.getMovieRecommendations(872585);
 	console.log(data);
 };
 </script>
