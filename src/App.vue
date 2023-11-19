d6dc933085228c964c6f1a04ee4d3fce1d958d79
DoubledConG <992196871@qq.com>
2023-11-19 22:27:10 +0800

    feat: add simple footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
+import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
 </script>
 
 <template>
 	<Header />
 	<div class="main">哈哈哈哈哈哈</div>
+	<Footer />
 </template>
 
 <style scoped lang="less">
 .main {
-	height: 5000px;
+	height: 300px;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
 <template>
-	<div>Footer</div>
+	<footer>
+		<div class="container">
+			@{{ new Date().getFullYear() }} made with
+			<i class="icon fas fa-heart" />
+			by DoubledConG
+		</div>
+	</footer>
 </template>
 <script setup lang="ts"></script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.container {
+	padding: 0.5rem;
+	font-size: 0.75rem;
+	text-align: center;
+	.icon {
+		color: #ff0808;
+	}
+}
+</style>
