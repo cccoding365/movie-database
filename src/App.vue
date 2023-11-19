bd5cf551336bb493387880cb7d85fe4686bf2c49
DoubledConG <992196871@qq.com>
2023-11-19 19:42:22 +0800

    feat: development navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,14 @@
 <script setup lang="ts">
-import * as Apis from "@/apis";
-
-const onGetMovies = async () => {
-	const data = await Apis.getMovies({
-		filter: "popular",
-		page: 1,
-	});
-	console.log("getMovies", data);
-};
-
-const onGetGenres = async () => {
-	const data = await Apis.getGenres();
-	console.log(data);
-};
-
-const onSearchMovies = async () => {
-	const data = await Apis.searchMovies({
-		query: "叶问",
-		page: 1,
-	});
-	console.log(data);
-};
-
-const onGetMovie = async () => {
-	const data = await Apis.getMovie(872585);
-	console.log(data);
-};
-
-const onMovieCredits = async () => {
-	const data = await Apis.getMovieCredits(872585);
-	console.log(data);
-};
-
-const onGetMovieImages = async () => {
-	const data = await Apis.getMovieImages(872585);
-	console.log(data);
-};
-
-const onGetMovieRecommendations = async () => {
-	const data = await Apis.getMovieRecommendations(872585);
-	console.log(data);
-};
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-	<h1>Movie database</h1>
-	<button @click="onGetMovies">getMovies</button>
-	<button @click="onGetGenres">getGenres</button>
-	<button @click="onSearchMovies">searchMovies</button>
-	<button @click="onGetMovie">getMovie</button>
-	<button @click="onMovieCredits">getMovieCredits</button>
-	<button @click="onGetMovieImages">getMovieImages</button>
-	<button @click="onGetMovieRecommendations">getMovieRecommendations</button>
+	<Header />
+	<div class="main"></div>
 </template>
 
 <style scoped lang="less">
-h1 {
-	color: #fff;
-	font-family: "Ubuntu", sans-serif;
-}
-button {
-	padding: 5px 10px;
-	color: white;
-	font-weight: bold;
-	background-color: lightblue;
-	&:nth-child(odd) {
-		background-color: lightpink;
-	}
+.main {
+	height: 1000px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,44 @@
 <template>
-	<div>Header</div>
+	<div class="container" :class="{ fixed: y > 10 }">
+		<img class="logo" src="/favicon.svg" alt="Movie Database" />
+		<div class="title">MovieDatabase</div>
+		<i class="more-icon fas fa-bars" />
+	</div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="less"></style>
+
+<script setup lang="ts">
+import { useScroll } from "@vueuse/core";
+
+const { y } = useScroll(window);
+</script>
+
+<style scoped lang="less">
+.container {
+	width: 100%;
+	display: flex;
+	align-items: center;
+	padding: 2.1333vw;
+
+	&.fixed {
+		position: fixed;
+		z-index: 9999;
+		background-color: #333;
+	}
+
+	.logo {
+		width: 8.5333vw;
+		height: 8.5333vw;
+		margin-right: 2.1333vw;
+	}
+	.title {
+		color: #fff;
+		font-size: 4.8vw;
+		font-weight: bold;
+	}
+	.more-icon {
+		color: #fff;
+		font-size: 6.4vw;
+		margin-left: auto;
+	}
+}
+</style>
