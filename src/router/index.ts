e0255c797f96cf508b53620fd279e9ace1af6d9e
DoubledConG <992196871@qq.com>
2023-11-21 21:01:44 +0800

    feat: develop the movie detail page

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+	{ path: "/", component: () => import("../views/Home.vue") },
+	{
+		path: "/movie/:id",
+		component: () => import("../views/movie/Detail.vue"),
+		props: true,
+	},
+];
+
+export default createRouter({
+	history: createWebHistory(),
+	routes,
+});
---
+++ src/views/movie/Detail.vue
@@ -0,0 +1,120 @@
+<template>
+	<div class="movie-detail">
+		<img
+			class="movie-detail-backdrop"
+			:src="MOVIE_DB_IMAGE_URL.large + MovieDetail.backdrop_path"
+		/>
+		<div class="movie-detail-content">
+			<div class="movie-detail-title">{{ MovieDetail.title }}</div>
+
+			<div class="tagline">{{ MovieDetail.tagline }}</div>
+
+			<div class="section">
+				<i class="icon fas fa-clock" />
+				<span> {{ MovieDetail.runtime }} minutes </span>
+				<i class="icon fas fa-star" />
+				<span>
+					{{ MovieDetail.vote_average.toFixed(1) }} ( TMDB )
+				</span>
+			</div>
+
+			<div class="section">
+				<div class="release-date">
+					<div class="label">Release date</div>
+					<div class="value">{{ MovieDetail.release_date }}</div>
+				</div>
+				<div class="genre">
+					<div class="label">Genre</div>
+					<div class="value">
+						<span v-for="item in MovieDetail.genres">
+							{{ item.name }}
+						</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="overview">
+				<div class="overview-title">Overview</div>
+				<div class="overview-content">{{ MovieDetail.overview }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { MOVIE_DB_IMAGE_URL } from "@/configs/image";
+import MovieDetail from "@/apis/movie-detail.json";
+import { onMounted } from "vue";
+// const props = defineProps({
+// 	id: String,
+// });
+
+onMounted(() => {
+	console.log(MovieDetail);
+});
+</script>
+
+<style lang="less" scoped>
+.movie-detail {
+	&-backdrop {
+		width: 100%;
+	}
+	&-title {
+		margin: 0.5rem 0;
+		font-size: 1.5rem;
+		color: #fff;
+	}
+
+	.tagline {
+		font-size: 0.75rem;
+		line-height: 1rem;
+	}
+
+	&-content {
+		padding: 0 1rem;
+		font-size: 0.875rem;
+		color: #bcbcbc;
+		.section {
+			display: flex;
+			padding: 0.75rem 0;
+			margin-bottom: 0.5rem;
+			border-bottom: 1px solid #201f28;
+			.icon {
+				&:not(:nth-child(1)) {
+					margin-left: 1rem;
+				}
+				margin-right: 0.25rem;
+			}
+			.release-date,
+			.genre {
+				flex: 1;
+				.label {
+					color: #fff;
+					font-size: 1.125rem;
+					margin-bottom: 0.5rem;
+				}
+				.value {
+					span {
+						border-radius: 999px;
+						padding: 0.2rem 0.5rem;
+						border: 1px solid #999;
+						font-size: 0.6rem;
+						margin-right: 0.5rem;
+					}
+				}
+			}
+		}
+	}
+
+	.overview {
+		&-title {
+			color: #fff;
+			font-size: 1.125rem;
+			margin-bottom: 0.5rem;
+		}
+		&-content {
+			line-height: 1.125rem;
+		}
+	}
+}
+</style>
