e75f42f8e7a8fa1ab7ffab1d3f313b64ffe80669
DoubledConG <992196871@qq.com>
2023-11-22 23:14:55 +0800

    feat: change fake data to The Wandering Earth II

--- src/views/movie/Detail.vue
+++ src/views/movie/Detail.vue
@@ -182,12 +182,14 @@ onMounted(() => {
 					margin-bottom: 0.5rem;
 				}
 				.value {
+					display: flex;
+					flex-wrap: wrap;
 					span {
 						border-radius: 999px;
 						padding: 0.2rem 0.5rem;
 						border: 1px solid #999;
 						font-size: 0.6rem;
-						margin-right: 0.5rem;
+						margin: 0 0.5rem 0.5rem 0;
 					}
 				}
 			}
