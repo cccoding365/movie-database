0c5d76b21d1bded27a6a9173f9fd0e9d5bf17d70
DoubledConG <992196871@qq.com>
2023-12-03 21:30:50 +0800

    styles: adjust the movie card styles

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,13 +2,12 @@
 	<div class="poster">
 		<img
 			v-if="data.poster_path"
-			class="poster"
 			:src="MOVIE_DB_IMAGE_URL.medium + data.poster_path"
 			alt="movie-poster"
 			@error="e => ((<HTMLImageElement>e.target).style.display = 'none')"
 		/>
 	</div>
-	<div class="original_title" :title="(data.title as string)">
+	<div class="original_title" :title="data.title">
 		{{ data.title }}
 	</div>
 	<div class="release_year">( {{ data.release_date.split("-")[0] }} )</div>
@@ -18,9 +17,9 @@
 import { MOVIE_DB_IMAGE_URL } from "@/configs/image";
 
 interface IMovieItem {
-	poster_path: String;
-	title: String;
-	release_date: String;
+	poster_path: string;
+	title: string;
+	release_date: string;
 }
 
 defineProps<{
@@ -31,17 +30,21 @@ defineProps<{
 <style lang="less" scoped>
 .poster {
 	display: flex;
+	align-items: center;
 	border-radius: 0.5rem;
+	position: relative;
 	img {
 		width: 100%;
 	}
 	&::after {
 		content: "no poster";
-		flex: 1;
 		font-size: 1.375rem;
+		text-align: center;
+		width: 100%;
 		font-style: oblique;
 		text-transform: capitalize;
-		padding-top: 100%;
+		padding-bottom: 100%;
+		position: absolute;
 		color: #999;
 		font-weight: bold;
 		z-index: -1;
